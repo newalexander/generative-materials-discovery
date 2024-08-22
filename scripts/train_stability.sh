d4b394b73e61476ba80a475d74d5d2c31c0a5480
Alexander New <alexander.new0@gmail.com>
2024-08-22 12:39:50 -0400

    Update train_stability.sh
    point to correct file locations

--- scripts/train_stability.sh
+++ scripts/train_stability.sh
@@ -2,8 +2,8 @@
 
 # Licensed under the Apache License, Version 2.0
 
-ROOT_DIR=...  # point to where `mp_stability/` is located
-ID_PROP=...  # point to where `Ed_tern.csv` is located
+ROOT_DIR=...  # point to directory where ALIGNN training data CIFS are located
+ID_PROP=...  # point to where `materials-discovery/data/alignn/Ed_tern.csv` is located
 OUTPUT_DIR=... # point to wherever you want to save the model
 
 python alignn/alignn/train_folder.py \
